04f6da47150b16c004f8aa9f795a7c44959a937b
Kristian Nielsen <knielsen@knielsen-hq.org>
2013-02-10 17:11:31 +0100

    Implement animation that allows to write individual LEDs directly inline in the sketch.

--- arduino_cube_zapro_5x5x5.ino
+++ arduino_cube_zapro_5x5x5.ino
@@ -23,6 +23,45 @@ static const uint16_t led_map_zapro_5x5x5[] PROGMEM = {
   108, 119, 114, 109, 120, 115, 110, 0x8000,
 };
 
+static const uint8_t frame_L_A[] PROGMEM = {
+  B00000, B10000, B00000, B00100, B00000,
+  B00000, B10000, B00000, B01010, B00000,
+  B00000, B10000, B00000, B11111, B00000,
+  B00000, B10000, B00000, B10001, B00000,
+  B00000, B11111, B00000, B10001, B00000
+};
+
+static const uint8_t frame_PER[] PROGMEM = {
+  B11100, B00000, B11111, B00000, B11110,
+  B10010, B00000, B10000, B00000, B10001,
+  B11100, B00000, B11100, B00000, B11110,
+  B10000, B00000, B10000, B00000, B10100,
+  B10000, B00000, B11111, B00000, B10010
+};
+
+
+static void
+anim_show_binary_array(uint8_t f, uint16_t counter, uint16_t data)
+{
+  const uint8_t *ptr = (const uint8_t *)data;
+  uint8_t x, y, z;
+
+  fast_clear(f, 0);
+  for (z = 0; z < 5; ++z)
+  {
+    for (y = 0; y < 5; ++y)
+    {
+      uint8_t row = pgm_read_byte(&ptr[20-z*5+y]);
+      for (x = 0; x < 5; ++x)
+      {
+        if (row & (1 << (4-x)))
+          pixel5(f, x, y, z, 15);
+      }
+    }
+  }
+}
+
+
 static void
 anim_my_test(uint8_t f, uint16_t counter, uint16_t data)
 {
@@ -38,6 +77,8 @@ anim_my_test(uint8_t f, uint16_t counter, uint16_t data)
 }
 
 static const struct ledcube_anim animation[] PROGMEM = {
+  { anim_show_binary_array, (uint16_t)frame_PER, 2400 },
+  { anim_show_binary_array, (uint16_t)frame_L_A, 2400 },
   { anim_my_test, 500, 1500 },
   { anim_scan_plane_5, 15, 1200 },
   { anim_cosine_plane5, 22, 800 },
@@ -50,6 +91,6 @@ static const struct ledcube_anim animation[] PROGMEM = {
 };
 
 int main(int argc, char *argv[]) {
-  run_cube(led_map_zapro_5x5x5, 32, 2, animation);
+  run_cube(led_map_zapro_5x5x5, 12, 2, animation);
 }
 
